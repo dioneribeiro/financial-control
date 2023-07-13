876a4cca77c8d3ddbd6c7b8aead25769380efa17
Dione Ribeiro Niza <dioneribeiron@gmail.com>
2023-07-13 19:19:17 -0300

    primeiro commit do projeto

---
+++ app/models/movement.rb
@@ -0,0 +1,26 @@
+class Movement < ApplicationRecord
+  enum :category, { saida: 'saida', entrada: 'entrada' }
+
+  belongs_to :user
+
+  validates :date, comparison: { less_than_or_equal_to: proc { Date.current } }
+  validates :description, presence: true
+  validates :description, length: { maximum: 150 }
+  validates :value, presence: true
+  validates :category, presence: true
+  validate :valida_se_existe_saldo
+
+  def self.saldo_atual
+    self.entrada.sum(:value) - self.saida.sum(:value)
+  end
+
+  private
+
+  def valida_se_existe_saldo
+    return unless user
+    return if entrada?
+    return if value.to_f <= user.movements.saldo_atual
+
+    errors.add :value, 'não há saldo suficiente'
+  end
+end
